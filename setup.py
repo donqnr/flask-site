1f9810e566de7c294f8b3b1bb1a0dc48691ad85f
donqnr <danconar@gmail.com>
2021-12-07 12:24:23 +0200

    stuf

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='flasksite',
-    version='1.0.0',
+    version='0.5.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
@@ -10,5 +10,6 @@ setup(
         'flask',
         'flask-admin',
         'flask-sqlalchemy'
+        'flask-login'
     ],
 )
